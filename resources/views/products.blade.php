fd9bd9059d96cb8a8e0f568b68db30791bc6ea55
Defri <defrilugas46@gmail.com>
2025-05-12 12:21:31 +0700

    apa bae ah

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -383,7 +383,7 @@
 
         <div class="product-card" data-category="Sayuran Daun">
           <img
-            src="https://images.unsplash.com/photo-1592424002053-21f369ad7fdb"
+            src="https://images.unsplash.com/photo-1662318183265-5bbe6578d1b0?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Kangkung"
           />
           <h3 class="product-name">Kangkung</h3>
